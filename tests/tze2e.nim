e9b58c3e20c7814a1005afe5a077faae01e01ba7
hamidb80 <hr.bolouri@gmail.com>
2026-01-10 13:56:49 +0330

    .

--- tests/tze2e.nim
+++ tests/tze2e.nim
@@ -1,28 +1,20 @@
-import std/[
-  unittest,
-  os,
-  strformat,
-]
+import std/[unittest, os, strformat]
 import md
 
 
 suite "E2E":
-  # for (t, path) in walkDir "./tests/cases/":
-  for (t, path) in walkDir "../tests/cases/":
+  for (t, path) in walkDir "./tests/cases/":
     if t == pcFile: 
-      # test path:
-        # try:
+      test path:
+        try:
           let 
             content = readFile path
             doc     = parseMarkdown content
             newdoc  = attachNextCommentOfFigAsDesc doc
             fname   = path.splitFile.name
 
-          # writeFile fmt"./dist/{fname}.xml", toXml newdoc
-          # writeFile fmt"./dist/{fname}.tex", toTex newdoc
+          writeFile fmt"./dist/{fname}.xml", toXml newdoc
+          writeFile fmt"./dist/{fname}.tex", toTex newdoc
           
-          writeFile fmt"../dist/{fname}.xml", toXml newdoc
-          writeFile fmt"../dist/{fname}.tex", toTex newdoc
-
-        # except CatchableError:
-        #   check false
+        except CatchableError:
+          check false
